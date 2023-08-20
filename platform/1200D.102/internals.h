46fbb74fc97d284614a8bc6b3ec5ce307cd8eb43
Bhargav Das <bhargavdas100@gmail.com>
2023-08-21 00:37:07 +0530

    1200D: Sync internal.h with 1100D since both are almost similar
    This also solves the following errors
    
    ../../src/task_utils.c: In function 'get_current_task_name':
    ../../src/task_utils.c:14:28: error: dereferencing pointer to incomplete type 'struct task'
             return current_task->name;
                                ^~
    ../../src/task_utils.c: In function 'get_task_name_from_id':
    ../../src/task_utils.c:49:12: error: variable 'task_attr' has initializer but incomplete type
         struct task_attr_str task_attr = {0};
    
    After the defining the following configs
    
    Signed-off-by: Bhargav Das <bhargavdas100@gmail.com>

--- platform/1200D.102/internals.h
+++ platform/1200D.102/internals.h
@@ -16,6 +16,9 @@
 /** This camera runs DryOS **/
 //~ #define CONFIG_VXWORKS
 
+/** This camera has a DIGIC IV chip */
+#define CONFIG_DIGIC_IV
+
 /** This camera has an APS-C sensor */
 //~ #define CONFIG_FULLFRAME
 
@@ -66,11 +69,11 @@
 //~ #define CONFIG_ZOOM_BTN_NOT_WORKING_WHILE_RECORDING
 
 /** We can redirect the display buffer to some arbitrary address, just by changing YUV422_LV_BUFFER_DISPLAY_ADDR **/
-//~ #define CONFIG_CAN_REDIRECT_DISPLAY_BUFFER_EASILY
-//~ #define CONFIG_CAN_REDIRECT_DISPLAY_BUFFER
+#define CONFIG_CAN_REDIRECT_DISPLAY_BUFFER_EASILY
+#define CONFIG_CAN_REDIRECT_DISPLAY_BUFFER
 
 /** Therefore, we can implement display filters (features that alter the LiveView image in real-time) **/
-//~ #define CONFIG_DISPLAY_FILTERS
+#define CONFIG_DISPLAY_FILTERS
 
 /** We can override ISO on a per-frame basis, by changing FRAME_ISO (e.g. for HDR video or gradual exposure) **/
 #define CONFIG_FRAME_ISO_OVERRIDE
@@ -81,8 +84,8 @@
 /** We can't change ExpSim from ML :( **/
 //~ #define CONFIG_EXPSIM
 
-/** We can't playback sounds via ASIF DMA **/
-//~ #define CONFIG_BEEP
+/** We can playback sounds via ASIF DMA **/
+#define CONFIG_BEEP
 
 /** This camera has trouble saving Kelvin and/or WBShift in movie mode, so ML has to do this instead **/
 #define CONFIG_WB_WORKAROUND
@@ -92,11 +95,11 @@
 
 /** We can use DMA_MEMCPY but it has no real benefit **/
 //~ #define CONFIG_DMA_MEMCPY
-/** We do know how to use edmac_memcpy. This one is really fast (600MB/s!) */
-#define CONFIG_EDMAC_MEMCPY
+/** We don't know how to use edmac_memcpy. This one is really fast (600MB/s!) */
+//#define CONFIG_EDMAC_MEMCPY
 
-/** We should warn the user if movie exposure is Auto **/
-#define CONFIG_MOVIE_AE_WARNING
+/** We should't warn the user if movie exposure is Auto **/
+//~ #define CONFIG_MOVIE_AE_WARNING
 
 /** We can display some extra info in photo mode (not LiveView) **/
 #define CONFIG_PHOTO_MODE_INFO_DISPLAY
@@ -120,17 +123,23 @@
 #define CONFIG_MONO_MIC
 
 /** This camera has a low-resolution display, may require some antialiasing tricks for icons/fonts */
-//~ #define CONFIG_LOW_RESOLUTION_DISPLAY
+#define CONFIG_LOW_RESOLUTION_DISPLAY
 
 /** This camera uses the exposure comp button to open ML menu */
 #define CONFIG_MENU_WITH_AV
 
-/** We have access to raw data in both photo mode and in LiveView */
-#define CONFIG_RAW_LIVEVIEW
+/** We have access to raw data in LiveView and Photo mode */
 #define CONFIG_RAW_PHOTO
+#define CONFIG_RAW_LIVEVIEW
+#define CONFIG_EDMAC_MEMCPY
+#define CONFIG_EDMAC_RAW_SLURP
 
 /** There are no manual exposure controls in movie mode => we need expo override */
-//~ #define CONFIG_NO_MANUAL_EXPOSURE_MOVIE
+#define CONFIG_NO_MANUAL_EXPOSURE_MOVIE
 
 /** Use a patched LiveViewApp dialog hander to hide Canon bottom bar */
-#define CONFIG_LVAPP_HACK_PATCH
+#define CONFIG_LVAPP_HACK_RELOC
+
+#define CONFIG_NEW_TASK_STRUCTS
+#define CONFIG_TASK_STRUCT_V2
+#define CONFIG_TASK_ATTR_STRUCT_V2
